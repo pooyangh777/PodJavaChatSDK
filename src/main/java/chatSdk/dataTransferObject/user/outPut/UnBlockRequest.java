cb215a0c147cd973988491cca8b9700a867d90a0
pooyangh777 <pooyangq@gmail.com>
2023-05-06 11:00:42 +0430

    refactor_class_unBlock_request

--- src/main/java/chatSdk/dataTransferObject/user/outPut/UnBlockRequest.java
+++ src/main/java/chatSdk/dataTransferObject/user/outPut/UnBlockRequest.java
@@ -2,22 +2,26 @@ package chatSdk.dataTransferObject.user.outPut;
 
 import chatSdk.dataTransferObject.chat.ChatMessageType;
 import chatSdk.dataTransferObject.thread.outPut.BaseRequest;
+import com.google.gson.Gson;
 
 public class UnBlockRequest extends BaseRequest {
-    private long blockId;
-    private long userId;
-    private long contactId;
+    private final Long blockId;
+    private final Long userId;
+    private final Long contactId;
+    private final Long threadId;
 
     public UnBlockRequest(Builder builder) {
-        super(builder.uniqueId, builder.subjectId, ChatMessageType.UNBLOCK);
+        super(builder.uniqueId, ChatMessageType.UNBLOCK);
         this.blockId = builder.blockId;
         this.contactId = builder.contactId;
         this.userId = builder.userId;
+        this.threadId = builder.threadId;
     }
 
     @Override
     public String getChatMessageContent() {
-        return null;
+        Gson gson = new Gson();
+        return gson.toJson(this);
     }
 
     public static class Builder {
@@ -25,7 +29,7 @@ public class UnBlockRequest extends BaseRequest {
         private long userId;
         private long contactId;
         private String uniqueId;
-        private Long subjectId;
+        private Long threadId;
 
         public long getBlockId() {
             return blockId;
@@ -63,12 +67,12 @@ public class UnBlockRequest extends BaseRequest {
             return this;
         }
 
-        public Long getSubjectId() {
-            return subjectId;
+        public Long getThreadId() {
+            return threadId;
         }
 
-        public Builder setSubjectId(Long subjectId) {
-            this.subjectId = subjectId;
+        public Builder setThreadId(Long threadId) {
+            this.threadId = threadId;
             return this;
         }
 
