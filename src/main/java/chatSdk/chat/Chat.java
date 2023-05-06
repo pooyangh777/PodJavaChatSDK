1bb1b2d9e94d9aa6b13b0fbc5be2dabdb91a121f
pooyangh777 <pooyangq@gmail.com>
2023-05-06 16:46:00 +0430

    refactor_add_participants

--- src/main/java/chatSdk/chat/Chat.java
+++ src/main/java/chatSdk/chat/Chat.java
@@ -4948,6 +4948,11 @@ public class Chat implements AsyncListener {
         return request.getUniqueId();
     }
 
+    public String addParticipants2(AddParticipantsRequest request) {
+        sendAsyncMessage2(request);
+        return request.getUniqueId();
+    }
+
     private void sendAsyncMessage2(BaseRequest request) {
         if (state == ChatState.ChatReady) {
             ChatMessage chatMessage = new ChatMessage();
