2faf4814f59509a4fc1f5b4610ac27bac145f3b9
pooyangh777 <pooyangq@gmail.com>
2023-05-09 11:44:02 +0430

    refactor_seen_message_list

--- src/main/java/chatSdk/chat/Chat.java
+++ src/main/java/chatSdk/chat/Chat.java
@@ -4993,6 +4993,11 @@ public class Chat implements AsyncListener {
         return request.getUniqueId();
     }
 
+    public String seenMessageList(SeenMessageListRequest request) {
+        sendAsyncMessage2(request);
+        return request.getUniqueId();
+    }
+
     private void sendAsyncMessage2(BaseRequest request) {
         if (state == ChatState.ChatReady) {
             ChatMessage chatMessage = new ChatMessage();
