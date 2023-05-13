18bc97e8e1a7061cdcc0a596b37c7aec20869a30
pooyangh777 <pooyangq@gmail.com>
2023-05-13 12:12:32 +0430

    add_current_user_roles

--- src/main/java/chatSdk/chat/Chat.java
+++ src/main/java/chatSdk/chat/Chat.java
@@ -5018,6 +5018,11 @@ public class Chat implements AsyncListener {
         return request.getUniqueId();
     }
 
+    public String currentUserRoles(GeneralRequest request) {
+        sendAsyncMessage2(request);
+        return request.getUniqueId();
+    }
+
     private void sendAsyncMessage2(BaseRequest request) {
         if (state == ChatState.ChatReady) {
             ChatMessage chatMessage = new ChatMessage();
